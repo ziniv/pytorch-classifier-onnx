2009e8a1bf359c18c50850cd08eb97c590f19694
yj7840 <lgenney@gmail.com>
2023-03-14 21:06:15 +0900

    classifier module

--- module/classifier.py
+++ module/classifier.py
@@ -1,70 +1,43 @@
-import os 
-import argparse
 import pytorch_lightning as pl
 
-# when using 
-from pytorch_lightning.callbacks import LearningRateMonitor, ModelCheckpoint
-from pytorch_lightning.loggers import TensorBoardLogger
-from pytorch_lightning.plugins import DDPPlugin
-
-from datasets import *
-
-# for pytorch lightning test
-# todo : create classification pytorch lightning module which to customize or evaluate backbones.
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
-import torchvision.models as models
 from torch.optim import SGD, Adam
-from torch.utils.data import DataLoader
 from torchmetrics import Accuracy
 
-class ResNetClassifier(pl.LightningModule):
-    resnets = {
-        18: models.resnet18,
-        34: models.resnet34,
-        50: models.resnet50,
-        101: models.resnet101,
-        152: models.resnet152,
+class Classifier(pl.LightningModule):
+    optimizers = {
+        'adam' : Adam,
+        'sgd' : SGD
     }
-    
-    optimizers = {"adam": Adam, "sgd": SGD}
-    
-    def __init__(self, num_classes, resnet_version, train_path, val_path, test_path=None, optimizer="adam", lr=1e-3, batch_size=16, transfer=True, tune_fc_only=False):
-        super(ResNetClassifier, self).__init__()
-        self.num_classes = num_classes
-        self.lr = lr
-        self.batch_size = batch_size
-        self.optimizer = self.optimizers[optimizer]
+    def __init__(self, model, cfg):
+        super(Classifier, self).__init__()
+        self.model = model
+        
+        self.num_classes = cfg['classes']
         self.loss_fn = (
-            nn.BCEWithLogitsLoss() if num_classes == 1 else nn.CrossEntropyLoss()
+            nn.BCEWithLogitsLoss() if self.num_classes == 1 else nn.CrossEntropyLoss()
         )
         self.acc = Accuracy(
-            task="binary" if num_classes == 1 else "multiclass", num_classes = num_classes
+            multiclass=False if self.num_classes == 1 else True, num_classes=self.num_classes
         )
-        self.resnet_model = self.resnets[resnet_version](pretrained=transfer)
-        linear_size = list(self.resnet_model.children())[-1].in_features
-        self.resnet_model.fc = nn.Linear(linear_size, num_classes)
-        
-    
-    def forward(self, x):
-        return self.resnet_model(x)
-    
-    def configure_optimizers(self):
-        return self.optimizer(self.parameters(), lr=self.lr)
+        self.optimizer = self.optimizers[cfg['optimizer']]
+        self.optimizer_opt = cfg['optimizer_options']
+        # if you ignore several options, input type of list
+        self.save_hyperparameters(ignore='model')
     
     def _step(self, batch):
         x, y = batch
-        preds = self(x)
+        preds = self.model(x)
 
         if self.num_classes == 1:
             preds = preds.flatten()
             y = y.float()
-
+        y = y.type(torch.LongTensor)
         loss = self.loss_fn(preds, y)
         acc = self.acc(preds, y)
         return loss, acc
-
+    
     def training_step(self, batch, batch_idx):
         loss, acc = self._step(batch)
         # perform logging
@@ -75,25 +48,13 @@ class ResNetClassifier(pl.LightningModule):
             "train_acc", acc, on_step=True, on_epoch=True, prog_bar=True, logger=True
         )
         return loss
-
-    def val_dataloader(self):
-        return self._dataloader(self.val_path)
-
+    
     def validation_step(self, batch, batch_idx):
         loss, acc = self._step(batch)
         # perform logging
         self.log("val_loss", loss, on_epoch=True, prog_bar=False, logger=True)
         self.log("val_acc", acc, on_epoch=True, prog_bar=True, logger=True)
-
-    def test_dataloader(self):
-        return self._dataloader(self.test_path)
-
-    def test_step(self, batch, batch_idx):
-        loss, acc = self._step(batch)
-        # perform logging
-        self.log("test_loss", loss, on_step=True, prog_bar=True, logger=True)
-        self.log("test_acc", acc, on_step=True, prog_bar=True, logger=True)
         
+    def configure_optimizers(self):
+        return self.optimizer(self.parameters(), **self.optimizer_opt)
     
-        # based on https://github.com/Stevellen/ResNet-Lightning/blob/master/resnet_classifier.py
-
